b1b55f12d2a07849465788b8446ab6f9191f777c
szlsay <409178030@qq.com>
2021-12-16 18:01:12 +0800

    首页-顶部通栏布局

---
+++ src/components/app-topnav.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="app-topnav">
+    <ul>
+      <li>
+        <a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a>
+      </li>
+      <li><a href="javascript:;">退出登录</a></li>
+      <li><a href="javascript:;">请先登录</a></li>
+      <li><a href="javascript:;">免费注册</a></li>
+      <li><a href="javascript:;">我的订单</a></li>
+      <li><a href="javascript:;">会员中心</a></li>
+      <li><a href="javascript:;">帮助中心</a></li>
+      <li><a href="javascript:;">关于我们</a></li>
+      <li>
+        <a href="javascript:;"><i class="iconfont icon-phone"></i>手机版</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppTopnav",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.app-topnav {
+  background-color: #333;
+  ul {
+    display: flex;
+    justify-content: flex-end;
+    height: 53px;
+    align-items: center;
+    li {
+      a {
+        display: inline-block;
+        padding: 0 15px;
+        color: #cdcdcd;
+        line-height: 1;
+        // border-left: 2px solid #666;
+        i {
+          font-size: 14px;
+          margin-right: 2px;
+        }
+        &:hover {
+          color: @xtxColor;
+        }
+      }
+
+      ~ li {
+        a {
+          border-left: 2px solid #666;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <nav>nav</nav>
+    <!-- <nav>nav</nav> -->
+    <app-topnav></app-topnav>
     <header>header</header>
     <main><router-view></router-view></main>
     <footer>footer</footer>
@@ -8,7 +9,11 @@
 </template>
 
 <script>
+import AppTopnav from "@/components/app-topnav.vue";
 export default {
+  components: {
+    AppTopnav,
+  },
   setup() {
     return {};
   },
