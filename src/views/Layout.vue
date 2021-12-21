d20118efb45c355d0a6630ea4d36af0cbfccf8f7
szlsay <409178030@qq.com>
2021-12-21 15:47:52 +0800

    首页-吸顶头部组件-组合API

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,6 +3,7 @@
     <!-- <nav>nav</nav> -->
     <app-topnav></app-topnav>
     <app-header></app-header>
+    <app-header-sticky></app-header-sticky>
     <main><router-view></router-view></main>
     <app-footer></app-footer>
   </div>
@@ -12,12 +13,14 @@
 import AppTopnav from "@/components/app-topnav.vue";
 import AppHeader from "@/components/app-header.vue";
 import AppFooter from "@/components/app-footer.vue";
+import AppHeaderSticky from "@/components/app-header-sticky.vue";
 import { useStore } from "vuex";
 export default {
   components: {
     AppTopnav,
     AppHeader,
     AppFooter,
+    AppHeaderSticky,
   },
   setup() {
     const store = useStore();
