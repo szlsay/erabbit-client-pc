bee78572be61db21b400e73aeb8de0ccbdc37ad3
szlsay <409178030@qq.com>
2021-12-22 10:10:41 +0800

    首页主体-轮播图-逻辑封装

--- src/components/library/xtx-carousel.vue
+++ src/components/library/xtx-carousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="xtx-carousel">
+  <div class="xtx-carousel" @mouseenter="stop()" @mouseleave="start()">
     <ul class="carousel-body">
       <li
         class="carousel-item"
@@ -29,7 +29,8 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { onUnmounted } from "vue";
 export default {
   name: "XtxCarousel",
   props: {
@@ -37,11 +38,71 @@ export default {
       type: Array,
       default: () => [],
     },
+    duration: {
+      type: Number,
+      default: 3000,
+    },
+    autoPlay: {
+      type: Boolean,
+      default: false,
+    },
   },
-  setup() {
+  setup(props) {
     const index = ref(0);
+    // 自动播放
+    let timer = null;
+    const autoPlayFn = () => {
+      clearInterval(timer);
+      timer = setInterval(() => {
+        index.value++;
+        if (index.value >= props.sliders.length) {
+          index.value = 0;
+        }
+      }, props.duration);
+    };
+    watch(
+      () => props.sliders,
+      (newVal) => {
+        // 有数据&开启自动播放，才调用自动播放函数
+        if (newVal.length && props.autoPlay) {
+          index.value = 0;
+          autoPlayFn();
+        }
+      },
+      { immediate: true }
+    );
+
+    // 鼠标进入停止，移出开启自动，前提条件：autoPlay为true
+    const stop = () => {
+      if (timer) clearInterval(timer);
+    };
+    const start = () => {
+      if (props.sliders.length && props.autoPlay) {
+        autoPlayFn();
+      }
+    };
+    // 上一张下一张
+    const toggle = (step) => {
+      const newIndex = index.value + step;
+      if (newIndex >= props.sliders.length) {
+        index.value = 0;
+        return;
+      }
+      if (newIndex < 0) {
+        index.value = props.sliders.length - 1;
+        return;
+      }
+      index.value = newIndex;
+    };
+    // 组件消耗，清理定时器
+    onUnmounted(() => {
+      clearInterval(timer);
+    });
     return {
       index,
+      stop,
+      start,
+      toggle,
     };
   },
 };
