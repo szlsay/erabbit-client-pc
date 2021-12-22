7e70f1867708e520a25994dfa69a23aeb7d2522b
szlsay <409178030@qq.com>
2021-12-22 15:05:41 +0800

    顶级类目-面包屑组件-高级

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,6 +1,8 @@
 import XtxSkeleton from './xtx-skeleton.vue'
 import XtxCarousel from './xtx-carousel.vue'
 import XtxMore from './xtx-more.vue'
+import XtxBread from './xtx-bread.vue'
+import XtxBreadItem from './xtx-bread-item.vue'
 import defaultImg from '@/assets/images/200.png'
 // 指令
 const defineDirective = (app) => {
@@ -28,6 +30,8 @@ export default {
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
     app.component(XtxMore.name, XtxMore)
+    app.component(XtxBread.name, XtxBread)
+    app.component(XtxBreadItem.name, XtxBreadItem)
     defineDirective(app)
   }
 }
--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Top-Category--{{ $route.params.id }}</div>
+  <div>
+    <!-- 面包屑 -->
+    <XtxBread>
+      <XtxBreadItem to="/">首页</XtxBreadItem>
+      <XtxBreadItem to="/category/1005000">电器</XtxBreadItem>
+      <XtxBreadItem>空调</XtxBreadItem>
+    </XtxBread>
+  </div>
 </template>
 
 <script>
