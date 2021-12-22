48a63fdd6ed496f4d2bd775f1446c638e47f740a
szlsay <409178030@qq.com>
2021-12-22 16:27:49 +0800

    二级类目-展示面包屑

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>Sub-Category--{{ $route.params.id }}</div>
+  <div class="sub-category">
+    <div class="container">
+      <!-- 面包屑 -->
+      <SubBread />
+    </div>
+  </div>
 </template>
 
 <script>
+import SubBread from "./components/sub-bread";
 export default {
-  name: "Sub-Category",
-  setup() {
-    return {};
-  },
+  name: "SubCategory",
+  components: { SubBread },
 };
 </script>
-
-<style lang="less" scoped>
-</style>
