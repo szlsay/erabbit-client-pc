3588ee9a960649f58ef5d9bb6b2ab726a3a53df4
szlsay <409178030@qq.com>
2021-12-22 17:22:08 +0800

    二级类目-结果区-排序组件

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,6 +5,12 @@
       <SubBread />
       <!-- 筛选区 -->
       <SubFilter />
+      <!-- 结果区域 -->
+      <div class="goods-list">
+        <!-- 排序 -->
+        <SubSort />
+        <!-- 列表 -->
+      </div>
     </div>
   </div>
 </template>
@@ -12,8 +18,16 @@
 <script>
 import SubBread from "./components/sub-bread";
 import SubFilter from "./components/sub-filter";
+import SubSort from "./components/sub-sort.vue";
 export default {
   name: "SubCategory",
-  components: { SubBread, SubFilter },
+  components: { SubBread, SubFilter, SubSort },
 };
 </script>
+<style scoped lang='less'>
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+}
+</style>
