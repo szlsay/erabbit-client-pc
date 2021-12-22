8e9cf51ff54f945fe2f4bc90e795dbbd9a7c78b0
szlsay <409178030@qq.com>
2021-12-22 16:23:26 +0800

    二级类目-处理跳转细节

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -76,7 +76,7 @@ export default {
     watch(
       () => route.params.id,
       (newVal) => {
-        newVal && getSubList();
+        if (newVal && `/category/${newVal}` === route.path) getSubList();
       },
       { immediate: true }
     );
