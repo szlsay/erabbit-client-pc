a83f2a4f9d7c6131c41a61b18306326143401339
szlsay <409178030@qq.com>
2021-12-22 15:15:56 +0800

    顶级类目-批量注册组件

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,8 +1,8 @@
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
-import XtxBread from './xtx-bread.vue'
-import XtxBreadItem from './xtx-bread-item.vue'
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
+// import XtxBread from './xtx-bread.vue'
+// import XtxBreadItem from './xtx-bread-item.vue'
 import defaultImg from '@/assets/images/200.png'
 // 指令
 const defineDirective = (app) => {
@@ -25,13 +25,18 @@ const defineDirective = (app) => {
   })
 }
 
+const importFn = require.context('./', false, /\.vue$/)
 export default {
   install(app) {
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
-    app.component(XtxBread.name, XtxBread)
-    app.component(XtxBreadItem.name, XtxBreadItem)
+    importFn.keys().forEach(key => {
+        const component = importFn(key).default
+        app.component(component.name, component)
+      })
+      // app.component(XtxSkeleton.name, XtxSkeleton)
+      // app.component(XtxCarousel.name, XtxCarousel)
+      // app.component(XtxMore.name, XtxMore)
+      // app.component(XtxBread.name, XtxBread)
+      // app.component(XtxBreadItem.name, XtxBreadItem)
     defineDirective(app)
   }
 }
