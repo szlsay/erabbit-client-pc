db6eb0af7d3001c33e5f0aa86ba9cecc99cb1314
szlsay <409178030@qq.com>
2021-12-22 11:23:46 +0800

    首页主体-热门品牌

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -10,6 +10,8 @@
         <HomeNew />
         <!-- 人气推荐 -->
         <HomeHot />
+        <!-- 热门品牌 -->
+        <HomeBrand />
       </div>
     </div>
   </div>
@@ -20,9 +22,10 @@ import HomeCategory from "./components/home-category";
 import HomeBanner from "./components/home-banner.vue";
 import HomeNew from "./components/home-new.vue";
 import HomeHot from "./components/home-hot.vue";
+import HomeBrand from "./components/home-brand.vue";
 export default {
   name: "HomePage",
-  components: { HomeCategory, HomeBanner, HomeNew, HomeHot },
+  components: { HomeCategory, HomeBanner, HomeNew, HomeHot, HomeBrand },
   setup() {
     return {};
   },
